4a89fe3248fdb395bbfa1d90f64560bdc0e4b2a1
zyansheep <zyansheep@protonmail.com>
2023-05-27 01:29:28 -0400

    rom loading, mem layout & cpu instr abstraction wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,151 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+mod rom;
+
+use std::{path::PathBuf, io::{self, Read}, fs};
+
+use clap::{Parser};
+use rom::{ROMError};
+use thiserror::Error;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Arguments {
+    /// Required binary path to run
+    bin_path: PathBuf,
 }
+
+#[derive(Error, Debug)]
+enum EmulatorError {
+    #[error("invalid rom format: {0}")]
+    ROMError(#[from] ROMError),
+}
+
+fn main() -> Result<(), EmulatorError> {
+    let args = Arguments::parse();
+
+    let path = args.bin_path;
+
+    println!("Loading binary: {:?}", path);
+
+    let mut state = State::new();
+
+    if let Err(err) = rom::load_rom(&path, &mut state) {
+        println!("failed to load rom: {err}");
+    }
+
+
+    Ok(())
+}
+
+bitflags::bitflags! {
+    #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
+    struct CpuFlags: u8 {
+        const Negative = 0b10000000;
+        const Overflow = 0b01000000;
+        const Decimal  = 0b00001000;
+        const Zero     = 0b00000010;
+        const Carry    = 0b00000001;
+        const InterruptDisable = 0b00000100;
+    }
+}
+
+/// Derived from: https://www.nesdev.org/wiki/CPU_registers and https://www.nesdev.org/wiki/Status_flags
+#[derive(Default, Debug)]
+struct CpuState {
+    /// Accumulator Register
+    a: u8,
+    /// X Index Register
+    x: u8,
+    /// Y Index Register
+    y: u8,
+    /// Status Flags
+    flags: CpuFlags,
+    /// Program Counter
+    pc: u16,
+    /// Stack Pointer
+    sp: u8,
+}
+
+/// Derived from: https://www.nesdev.org/wiki/CPU_memory_map
+pub struct State {
+    /// 2KB of internal RAM
+    ram: [u8; 0x0800],
+    /// Picture Processing Unit Registers
+    ppu: [u8; 0x0008],
+    /// Audio Processing Unit Registers
+    apu: [u8; 0x0018],
+    /// Testing registers
+    test: [u8; 0x0008],
+    cartridge: [u8; 0xBFE0],
+    /// CPU Registers
+    cpu: CpuState,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mem: [0u8; u16::MAX as usize],
+            cpu: Default::default(),
+        }
+    }
+    fn mem(&mut self, addr: u16) -> &mut u8 {
+        let idx = addr as usize;
+        &mut match addr {
+            /// Access internal RAM (is mirrored 4 times, total size 0x0800)
+            0x0000..=0x1FFF => self.ram[idx % 0x0800],
+            /// Access the PPU, repeats every 8 bytes until 0x1FF8
+            0x2000..=0x1FF8 => self.ppu[idx % 0x0008],
+            0x4000..=0x4017 => self.apu[idx],
+            0x4018..=0x401F => self.test[idx],
+            0x4020..=0xFFFF => self.cartridge[idx],
+        }
+    }
+    pub fn mem_get(&mut self, addr: u16) -> u8 {
+        self.mem(addr).clone()
+    }
+    pub fn mem_set(&mut self, addr: u16, val: u8) {
+        *self.mem(addr) = val;
+    }
+    /// Run the CPU
+    fn step(&mut self, instr_set: [fn(&mut State); 256]) {
+        // Get instruction data from memory
+        let instr_idx = self.mem_get(self.cpu.pc) as usize;
+        // Execute corresponding instruction
+        instr_set[instr_idx](self);
+    }
+}
+
+/// Structures that extract data from state.
+trait AddressingMode<DataType> {
+    fn load(state: &mut State) -> DataType;
+}
+
+/// Any object that implements both an InstructionType and an AddressingMode that supports the InstructionType's input.
+trait Instruction: InstructionType + AddressingMode<<Self as InstructionType>::Data> {
+    fn e(state: &mut State) {
+        let data = <Self as AddressingMode<<Self as InstructionType>::Data>>::load(state);
+        <Self as InstructionType>::execute(state, data);
+    }
+}
+
+
+/// Some type of instruction, irrespective of the addressing model.
+trait InstructionType {
+    type Data;
+    fn execute(state: &mut State, data: Self::Data);
+}
+/// Load to A
+struct LDA;
+impl InstructionType for LDA {
+    type Data = u8;
+    fn execute(state: &mut State, data: Self::Data) {
+        state.cpu.a = data;
+    }
+}
+/// Transfer A to X
+struct TAX;
+/// Transfer X to A
+struct TXA;
+struct PHA;
+struct PLA;
---
+++ src/rom.rs
@@ -0,0 +1,67 @@
+use std::{path::{PathBuf, Path}, fs};
+
+use bytes::Buf;
+use thiserror::Error;
+
+use crate::State;
+
+#[derive(Error, Debug)]
+pub enum ROMError {
+    #[error("failed to load INES rom file: {0:?}")]
+    IOError(#[from] std::io::Error),
+    #[error("invalid magic value: {0:x?} .nes file should have magic bytes [4e, 45, 53, 1a] at the beginning.")]
+    InvalidMagicValue([u8; 4]),
+}
+
+bitflags::bitflags! {
+    #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash)]
+    pub struct NESFlags67: u8 {
+        /// Enables 4-screen display (VRAM)
+        const FourScreen = 0b10000000;
+        /// Mapper register translation code / CHR caching - 512-bytes at $7000-$71FF (stored before PRG data)
+        const Trainer    = 0b01000000;
+        /// Cartridge contains battery-backed PRG RAM ($6000-7FFF) or other persistent memory
+        const BatteryRam = 0b00100000;
+        /// Mirroring: 0: horizontal (vertical arrangement) (CIRAM A10 = PPU A11)
+        ///            1: vertical (horizontal arrangement) (CIRAM A10 = PPU A10)
+        const Mirroring  = 0b00010000;
+        /// Enable NES2 backwards-compatible format of the NES format.
+        const NES2Format = 0b00001000;
+        const NES1Format = 0b00000100;
+        /// PlayChoice-10 (8 KB of Hint Screen data stored after CHR data)
+        const PlayChoice = 0b00000010;
+        /// VS Unisystem
+        const VSUnisys   = 0b00000001;
+    }
+}
+
+pub fn load_rom(path: &Path, state: &mut State) -> Result<(), ROMError> {
+    let file = fs::read(path)?;
+    let mut nes = file.as_slice();
+
+    /// Parse NES file format: https://www.nesdev.org/wiki/INES
+    if &nes[0..4] == b"NES\x1a" {
+        nes.advance(4);
+        let program_size = nes.get_u8() as usize * 16384;
+        let graphics_size = nes.get_u8() as usize * 8192;
+        let flags67 = nes.get_u16();
+        let mapper_upper = ((flags67 << 4) as u8) & 0b0000_1111; // Get upper mapper bits
+        let mapper = ((flags67 as u8) & 0b1111_0000) | mapper_upper; // Join upper with lower bits
+        let flags_upper = ((flags67 << 8) as u8) & 0b0000_1111;
+        let flags = NESFlags67::from_bits_retain(((flags67 as u8) << 4) | flags_upper);
+
+        println!("program_size: {program_size:?}, graphics_size: {graphics_size:?}");
+        println!("mapper: {mapper:#b}, flags: {flags:#b}");
+
+        // Not dealing with Trainer region for now...
+        if flags.contains(NESFlags67::Trainer) {
+            nes.advance(512);
+        }
+
+        state.cartridge.copy_from_slice(&nes[0..]);
+        Ok(())
+    } else {
+        // println!("{:x?} != {:x?}", &nes[0..4], b"NES\x1a");
+        Err(ROMError::InvalidMagicValue((&nes[0..4]).try_into().unwrap()))
+    }
+}
